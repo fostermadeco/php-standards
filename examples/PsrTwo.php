797f684a92f40bed708a3e61b4ccd7b5f1e0c1ff
Doug Grubba <douglas.grubba@gmail.com>
2013-11-24 21:39:43 -0500

    example class

---
+++ examples/PsrTwo.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace VisualChefs/Awesome/Forum;
+
+use Other/Awesome/OtherAwesomeForum;
+
+/**
+ * Another awesome forum we can use for our l4 projects!
+ *
+ * @author      Visual Chefs <code@visualchefs.com>
+ * @copyright   (C) 2013. All rights reserved.
+ * @version     1.0
+ */
+
+/**
+ * Another awesome forum we can use for our l4 projects!
+ *
+ * @category    Visual Chefs
+ * @package     Awesome
+ * @subpackage  Forum
+ * @since       1.0
+ */
+class Forum extends OtherAwesomeForum implements BasicForum {
+
+    /**
+     * Constructor
+     *
+     * @return  void
+     * @access  public
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Get all the forums.
+     *
+     * @param   boolean   $and_private    Get private forums as well.
+     * @return  array
+     * @access  public
+     */
+    public function all($and_private = false)
+    {
+    }
+
+    /**
+     * Protected method that does special stuff we want on its own.
+     *
+     * @return  void
+     * @access  protected
+     */
+    protected function _specialCaseFunc()
+    {
+    }
+
+}
